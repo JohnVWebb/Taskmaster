db4e58f8d36e913fe528ffdb44884346ba51fa0f
JohnVWebb <jvw1225@gmail.com>
2021-02-22 07:48:16 -0700

    edited comments

--- app/Services/SublistService.js
+++ app/Services/SublistService.js
@@ -8,21 +8,11 @@ export default class SublistService{
      ProxyState.on('sublists', saveState)
     }
     create(rawSublist) {
-         //same same
-         //let temp = ProxyState.sublists
-         //let newSublist = new Sublist(rawSublist)
-         //temp.push(newSublist)
-         //ProxyState.sublists = temp
-
         ProxyState.sublists = [new Sublist(rawSublist), ...ProxyState.sublists]
         console.log(ProxyState.sublists)
     }
     
     delete(sublistId) {
-        // let temp = ProxyState.sublists
-        // let sublistIndex = temp.findIndex(l => l.id == sublistId)
-        // temp.splice(sublistIndex, 1)
-        // ProxyState.sublists = temp
         ProxyState.sublists = ProxyState.sublists.filter(s => s.id != sublistId)
 
     }
